97604a5693b5f6f3c30763728a9c643639659b70
Umpz <46953365+PeerawatUmp@users.noreply.github.com>
2020-12-27 01:13:49 +0700

    Revert "try to restruct"
    This reverts commit 5237eb628f7612175c035eb646075a41135ccd34.

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/chonburichula/backend/api"
+	"backend/api"
 )
 
 func main() {
---
+++ mongostruct/go.mod
@@ -0,0 +1,5 @@
+module backend/mongostruct
+
+go 1.15
+
+require go.mongodb.org/mongo-driver v1.4.4
